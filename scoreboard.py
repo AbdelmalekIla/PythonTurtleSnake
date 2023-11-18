67571d1c875fb777904bb0bdc95acd283c386e11
abdelmalek <abdobenali887@gmail.com>
2023-11-18 01:21:21 +0100

    Implement Snake game using Python OOP and Turtle graphics--Updated
    - Create Snake class with movement, extension, and collision logic
    
    - Implement Food class for generating items
    
    - Integrate Scoreboard to track and display player's score
    
    - Handle collisions with food, walls, and snake's own tail
    
    - Display best score across game sessions

--- scoreboard.py
+++ scoreboard.py
@@ -1,19 +1,20 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
+
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open(r"C:\Users\PC\OneDrive\Bureau\BootCamp 100 Day\day21\new_fille.txt", mode='r') as file:
+        with open("new_fille.txt", mode='r') as file:
             self.hiegh_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
-        
 
     def update_scoreboard(self):
         self.clear()
@@ -22,7 +23,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.hiegh_score:
             self.hiegh_score=self.score
-            with open(r"C:\Users\PC\OneDrive\Bureau\BootCamp 100 Day\day21\new_fille.txt", mode='w') as file2:
+            with open("new_fille.txt", mode='w') as file2:
                 self.hiegh_score = file2.write(str(self.score))
         self.score = 0
         self.update_scoreboard()
